6d0f341c7152b0489cd2b3a32d25632389183071
elifmarali <elifmarali00@gmail.com>
2021-02-27 22:15:17 +0300

    gulpfile.js proje yolu degistirildi

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,85 @@
-const gulp = require("gulp");
-const browserSync = require("browser-sync").create();
-const sass = require("gulp-sass");
-const minifyCSS = require("gulp-csso");
-const minifyImg = require("gulp-imagemin");
-const minifyJS = require("gulp-uglify");
-const concat = require("gulp-concat");
-const autoprefixer = require("gulp-autoprefixer");
-const del = require("del");
-const fileName="1-search-filter-components";
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+const minifyCSS = require('gulp-csso')
+const minifyImg = require('gulp-imagemin')
+const minifyJS = require('gulp-uglify')
+const concat = require('gulp-concat')
+const autoprefixer = require('gulp-autoprefixer')
+const del = require('del')
+const fileName = '2-linkedin-redesign'
 
 //src/2-linkedin-redesign/**/
-gulp.task("browser-sync", () => {
+gulp.task('browser-sync', () => {
   browserSync.init({
     server: {
-      baseDir: "dist",
-    },
-  });
-});
+      baseDir: 'dist'
+    }
+  })
+})
 
-gulp.task("css", () => {
+gulp.task('css', () => {
   return gulp
     .src(`src/${fileName}/scss/**/*.scss`)
     .pipe(
       sass({
-        outputStyle: "nested",
+        outputStyle: 'nested',
         precision: 10,
-        includePaths: ["."],
-      }).on("error", sass.logError)
+        includePaths: ['.']
+      }).on('error', sass.logError)
     )
     .pipe(minifyCSS())
     .pipe(autoprefixer())
-    .pipe(concat("app.min.css"))
-    .pipe(gulp.dest("dist/css"))
-    .pipe(browserSync.stream());
-});
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
+})
 
-gulp.task("js", () => {
+gulp.task('js', () => {
   return gulp
     .src(`src/${fileName}/js/**/*.js`)
-    .pipe(concat("app.min.js"))
+    .pipe(concat('app.min.js'))
     .pipe(minifyJS())
-    .pipe(gulp.dest("dist/js"))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream())
+})
 
-gulp.task("html", () => {
+gulp.task('html', () => {
   return gulp
     .src(`src/${fileName}/**/*.html`)
-    .pipe(gulp.dest("dist"))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
+})
 
-gulp.task("font", () => {
+gulp.task('font', () => {
   return gulp
     .src(`src/${fileName}/**/*`)
-    .pipe(gulp.dest("dist"))
-    .pipe(browserSync.stream());
-});
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream())
+})
 
-gulp.task("img", () => {
+gulp.task('img', () => {
   gulp
     .src(`src/${fileName}/img/**/*`)
     .pipe(minifyImg())
-    .pipe(gulp.dest("dist/img"));
-});
+    .pipe(gulp.dest('dist/img'))
+})
 
-gulp.task("delete", () =>
-  del(["dist/css", "dist/js", "dist/img", "dist/**/*.html"])
-);
+gulp.task('delete', () =>
+  del(['dist/css', 'dist/js', 'dist/img', 'dist/**/*.html'])
+)
 
-gulp.task("watch", () => {
-  gulp.watch(`src/${fileName}/scss/**/*.scss`, gulp.task("css"));
-  gulp.watch(`src/${fileName}/js/**/*.js`, gulp.task("js"));
-  gulp.watch(`src/${fileName}/img/**/*.img`, gulp.task("img"));
-  gulp.watch(`src/${fileName}/**/*.html`, gulp.task("html"));
-});
+gulp.task('watch', () => {
+  gulp.watch(`src/${fileName}/scss/**/*.scss`, gulp.task('css'))
+  gulp.watch(`src/${fileName}/js/**/*.js`, gulp.task('js'))
+  gulp.watch(`src/${fileName}/img/**/*.img`, gulp.task('img'))
+  gulp.watch(`src/${fileName}/**/*.html`, gulp.task('html'))
+})
 
 gulp.task(
-  "default",
+  'default',
   gulp.series(
-    "delete",
-    gulp.parallel("html", "css", "js", "img", "font", "browser-sync", "watch")
+    'delete',
+    gulp.parallel('html', 'css', 'js', 'img', 'font', 'browser-sync', 'watch')
   )
-);
+)
