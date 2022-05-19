734cf33ce385771e53f913b0a8564ae8d4ac35d7
Elif Maralı <elifmarali00@gmail.com>
2022-05-19 18:10:03 +0300

    scss-exercise

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ const minifyJS = require('gulp-uglify')
 const concat = require('gulp-concat')
 const autoprefixer = require('gulp-autoprefixer')
 const del = require('del')
-const fileName = '8-color-palette-generator'
+const fileName = '10-price-cards'
 
 //src/2-linkedin-redesign/**/
 gulp.task('browser-sync', () => {
